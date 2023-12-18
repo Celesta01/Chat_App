a1b4c557a94a512fe344af094511eb6f64ba4610
unknown <aamir.k7199@gmail.com>
2023-12-18 18:02:13 +0530

    create project design home screen half

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(length: 4, vsync: this, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat App'),
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert))
+        ],
+        bottom: TabBar(controller: _controller, tabs: [
+          Tab(
+            icon: Icon(Icons.camera),
+          )
+        ]),
+      ),
+      body: const Center(child: Text('Home Screen')),
+    );
+  }
+}
